60a3a4d38e7cd2a5263bbaa9d7a03828e836323e
Sriram Karra <karra.etc@gmail.com>
2011-08-25 13:42:23 +0530

    Option to auto-save config after every change

--- tools/state.py
+++ tools/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ## Created	 : Tue Jul 19 13:54:53  2011
-## Last Modified : Thu Aug 25 13:21:51  2011
+## Last Modified : Thu Aug 25 13:41:15  2011
 ##
 ## Copyright 2011 Sriram Karra <karra.etc@gmail.com>
 ##
@@ -19,9 +19,15 @@ class Config:
     # There has to be a better way than this...
     sync_strs = [None, 'OUTLOOK', 'GOOGLE']
 
-    def __init__ (self, fn):
+    def __init__ (self, fn, sync_through=True):
+        """If sync_through is True, any change to the configuration is
+        immediately written back to the original disk file, otherwise
+        the user has to explicitly save to disk"""
+
         fi = None
         self.fn = fn
+        self.sync_through = False
+
         try:
             fi = open(fn, "r")
         except IOError, e:
@@ -35,12 +41,16 @@ class Config:
         self.state['conflict_resolve'] = getattr(
             self, self.inp['conflict_resolve'])
 
+        self.sync_through = sync_through
+
 
     def _get_prop (self, key):
         return self.state[key]
 
     def _set_prop (self, key, val):
         self.state[key] = val
+        if self.sync_through:
+            self.save()
 
     def get_gc_guid (self):
         return self._get_prop('GC_GUID')
@@ -53,6 +63,9 @@ class Config:
 
     def get_cr (self):
         return self._get_prop('conflict_resolve')
+
+    def set_gid (self, val):
+        return self._set_prop('gid', val)
 
     def get_gn (self):
         return self._get_prop('gn')
@@ -69,7 +82,7 @@ class Config:
     def set_resolve (self, val):
         return self._set_prop('conflict_resolve', val)
 
-    def save (self, fn):
+    def save (self, fn=None):
         if not fn:
             fn = self.fn
 
