8b9f38913b9eb741161935113e9245cdaf309379
Sriram Karra <karra.etc@gmail.com>
2012-03-16 12:14:36 +0530

    File format version 2 of the app state config
    The biggest change we are working on now is to support more than 2
    types of PIM databases. This requires some fundamental changes in
    how we capture the app configuraiton preferences as well as how
    the db sync status is tracked by the application.
    
    The app_state.json.example file, as always, is well documented
    and contains enough comments to make the file self-explanatory
    
    A number of general improvements have also been implemented in state.py
    whereby range checks are done for some fields, etc.

--- tools/state.py
+++ tools/state.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python
-
-## Created	 : Tue Jul 19 13:54:53  2011
-## Last Modified : Mon Nov 07 17:57:07 IST 2011
 ##
-## Copyright 2011 Sriram Karra <karra.etc@gmail.com>
+## Created       : Tue Jul 19 13:54:53 IST 2011
+## Last Modified : Fri Mar 16 12:13:36 IST 2012
+##
+## Copyright (C) 2011, 2012 Sriram Karra <karra.etc@gmail.com>
 ##
 ## Licensed under the GPL v3
 ## 
 
-import iso8601
-import demjson
-import logging, os, time
+import iso8601, demjson
+import logging, os, time, traceback
 
-SYNC_2_WAY     = 'Two Way Merge'
-SYNC_1_WAY_O2G = "One Way Sync - Outlook to Google"
-SYNC_1_WAY_G2O = "One Way Sync - Google  to Outlook"
+import utils
+
+sync_dirs = ['SYNC1WAY', 'SYNC2WAY']
+
+class GoutConfigError(Exception):
+    pass
 
 class Config:
-
-    OUTLOOK = 1
-    GOOGLE  = 2
-
-    # There has to be a better way than this...
-    sync_strs = [None, 'OUTLOOK', 'GOOGLE', 'TWOWAY']
-
     def __init__ (self, fn, sync_through=True):
         """If sync_through is True, any change to the configuration is
         immediately written back to the original disk file, otherwise
@@ -45,11 +39,16 @@ class Config:
 
         self.state = self.inp
 
-        self.state['conflict_resolve'] = getattr(
-            self, self.inp['conflict_resolve'])
+        # self.state['conflict_resolve'] = getattr(
+        #     self, self.inp['conflict_resolve'])
 
         self.sync_through = sync_through
 
+    ##
+    ## Helper routines
+    ##
+
+    ## Not dependent on sync state between a pair of PIMDs
 
     def _get_prop (self, key):
         return self.state[key]
@@ -59,57 +58,136 @@ class Config:
         if self.sync_through and sync:
             self.save()
 
-    def get_gc_guid (self):
-        return self._get_prop('GC_GUID')
+    ## Dependent on sync state between a pair of PIMDs. Each call has to
+    ## include two letter dbids. Invalid field access will throw a
+    ## GoutConfigError exeption
 
-    def get_gc_id (self):
-        return self._get_prop('GC_ID')
+    def _get_prop_ss (self, db1id, db2id, key):
+        dbs = ('%s%s%s' % (db1id, self.get_label_separator(), db2id))
+        try:
+            return self.state['sync_state'][dbs][key]
+        except KeyError, e:
+            raise GoutConfigError(('Invalid Config read: %s'
+                                   % (('%s:%s') % (dbs, key))))
 
-    def get_gid (self):
-        return self._get_prop('gid')
-
-    def get_cr (self):
-        return self._get_prop('conflict_resolve')
-
-    def set_gid (self, val, sync=True):
-        return self._set_prop('gid', val, sync)
-
-    def get_gn (self):
-        return self._get_prop('gn')
-
-    def get_last_sync_start (self):
-        return self._get_prop('last_sync_start')
+    def _set_prop_ss (self, db1id, db2id, key, val, sync=True):
+        dbs = '%s%s%s' % (db1id, self.get_label_separator(), db2id)
+        try:
+            self.state['sync_state'][dbs][key] = val
+            if self.sync_through and sync:
+                self.save()
+        except KeyError, e:
+            raise GoutConfigError(('Invalid Config write: %s'
+                                   % (('%s:%s') % (dbs, key))))
 
     def get_curr_time (self):
         return iso8601.tostring(time.time())
 
-    def set_last_sync_start (self, val=None, sync=True):
+    ##
+    ## get routines for 'readonly' config settings; so they don't have a
+    ## corresponding set method
+    ##
+
+    def get_olsync_guid (self):
+        return self._get_prop('olsync_guid')
+
+    def get_label_separator (self):
+        return self._get_prop('label_separator')
+
+    ##
+    ## get-set pairs for application modifiable config/state and not dependent
+    ## on specific PIMDB.
+    ##
+
+    def get_file_version (self):
+        return self._get_prop('file_version')
+
+    def set_file_version (self, val, sync=True):
+        return self._set_prop('file_version', val, sync)
+
+    def get_label_prefix (self):
+        return self._get_prop('label_prefix')
+
+    def set_label_prefix (self, val, sync=True):
+        return self._set_prop('label_prefix', val, sync)
+
+    def get_olsync_gid_next (self):
+        return self._get_prop('olsync_gid_next')
+
+    def set_olsync_gid_next (self, val, sync=True):
+        return self._set_prop('olsync_gid_next', val, sync)
+
+    ##
+    ## get-set pairs for application modifiable config/state specific to a
+    ## pair of synching PIMDBs..
+    ##
+
+    def get_last_sync_start (self, db1id, db2id):
+        return self._get_prop_ss(db1id, db2id, 'last_sync_start')
+
+    def set_last_sync_start (self, db1id, db2id, val=None, sync=True):
         if not val:
             val = iso8601.tostring(time.time())
-        return self._set_prop('last_sync_start', val, sync)
+        return self._set_prop_ss(db1id, db2id, 'last_sync_start', val, sync)
 
-    def set_gn (self, val, sync=True):
-        return self._set_prop('gn', val, sync)
+    def get_last_sync_stop (self, db1id, db2id):
+        return self._get_prop_ss(db1id, db2id, 'last_sync_stop')
 
-    def get_last_sync_stop (self):
-        return self._get_prop('last_sync_stop')
-
-    def set_last_sync_stop (self, val=None, sync=True):
+    def set_last_sync_stop (self, db1id, db2id, val=None, sync=True):
         if not val:
             val = iso8601.tostring(time.time())
-        return self._set_prop('last_sync_stop', val, sync)
+        return self._set_prop_ss(db1id, db2id, 'last_sync_stop', val, sync)
 
-    def get_resolve (self):
-        return self._get_prop('conflict_resolve')
+    def get_sync_dir (self, db1id, db2id):
+        val = self._get_prop_ss(db1id, db2id, 'sync_dir')
+        if not val in sync_dirs:
+            raise GoutConfigError(
+                ('Invalid value for sync_dir: %s%s%s[sync_dir]: %s' %
+                 (db1id, self.get_label_separator(), db2id, val)))
+        return val
 
-    def set_resolve (self, val, sync=True):
-        return self._set_prop('conflict_resolve', val, sync)
+    def set_sync_dir (self, db1id, db2id, val, sync=True):
+        if not val in sync_dirs:
+            raise GoutConfigError(
+                ('Invalid value for sync_dir: %s%s%s[sync_dir]: %s' %
+                 (db1id, self.get_label_separator(), db2id, val)))
 
-    def get_sync_dir (self):
-        return self._get_prop('sync_dir')
+        return self._set_prop_ss(db1id, db2id, 'sync_dir', val, sync)
 
-    def set_sync_dir (self, val, sync=True):
-        return self._set_prop('sync_dir', val, sync)
+    def get_conflict_resolve (self, db1id, db2id):
+        val = self._get_prop_ss(db1id, db2id, 'conflict_resolve')
+        if not val in [db1id, db2id]:
+            raise GoutConfigError(
+                ('Invalid value for: %s%s%s[conflict_resolve]: %s' %
+                 (db1id, self.get_label_separator(), db2id, val)))
+
+        return val
+
+    def set_conflict_resolve (self, db1id, db2id, val, sync=True):
+        if not val in [db1id, db2id]:
+            raise GoutConfigError(
+                ('Invalid value for: %s%s%s[conflic_resolve]: %s' %
+                 (db1id, self.get_label_separator(), db2id, val)))
+
+        return self._set_prop_ss(db1id, db2id, 'conflict_resolve', val, sync)
+
+    def get_group_ids (self, db1id, db2id, db, fname):
+        """Return the full map of Labels to Label/Group IDs for the requested
+        PIMDB. Essentially, this is used to query the identifiers used to
+        track the sync status of individual entries in the database by
+        labels. The caller will then have to find the label for the specific
+        group he is interested in."""
+
+        group_ids = self._get_prop_ss(db2id, db2id, 'group_ids')
+        return group_ids[db][fname]
+
+    def set_group_ids (self, db1id, db2id, db, fname, val, sync=True):
+        """val should be a dictionary of type {"label" : "value"} which will
+        be appended to the right group_ids dictionary"""
+
+        group_ids = self._get_prop_ss(db2id, db2id, 'group_ids')
+        group_ids[db].update({fname, val})
+        self._set_prop_ss(db1id, db2id, group_ids, sync)
 
     def save (self, fn=None):
         """fn should be the full absolute path. There is no guarantee
@@ -124,13 +202,114 @@ class Config:
             logging.critical('Error! Could not Open file (%s): %s', fn, e)
             return
 
-        save = self.get_resolve()
-        if save:
-            self.set_resolve(self.sync_strs[save], False)
-
-        fi.write(demjson.encode(self.state))
-
-        if save:
-            self.set_resolve(save, False)
+        fi.write(demjson.encode(self.state, compactly=False))
 
         fi.close()
+
+def main (argv=None):
+
+    import shutil, sys
+
+    if not argv:
+        argv = sys.argv
+
+    ## This module is for quick testing of the Config read/write
+    ## functionality. We will make a quick copy of the main example config
+    ## file into the current directory and start mucking with it.
+
+    src  = '../app_state.json.example'
+    dest = './app_state_test.json'
+    shutil.copyfile(src, dest)
+
+    config = Config(dest)
+
+    tcnt = 0
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'Label Separator: ', config.get_label_separator()
+    print 'olsync_guid: ', config.get_olsync_guid()
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'File Version: ', config.get_file_version()
+    print 'Setting File Version to 5'
+    config.set_file_version(5)
+    print 'File Version: ', config.get_file_version()    
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'Label Prefix: ', config.get_label_prefix()
+    print 'Setting Label Prefix to "Buffoon"'
+    config.set_label_prefix('Buffoon')
+    print 'Label Prefix: ', config.get_label_prefix()
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    val = config.get_olsync_gid_next()
+    print 'olsync_gid_next: ', val
+    print 'Incrementing olsync_gid_next'
+    config.set_olsync_gid_next(val+1)
+    print 'olsync_gid_next: ', config.get_olsync_gid_next()
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'bb:gc last_sync_start: ', config.get_last_sync_start('bb', 'gc')
+    print 'Resetting time to current time'
+    config.set_last_sync_start('bb', 'gc', config.get_curr_time())
+    print 'bb:gc last_sync_start: ', config.get_last_sync_start('bb', 'gc')
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'bb:gc last_sync_stop: ', config.get_last_sync_stop('bb', 'gc')
+    print 'Resetting time to current time'
+    config.set_last_sync_stop('bb', 'gc', config.get_curr_time())
+    print 'bb:gc last_sync_stop: ', config.get_last_sync_stop('bb', 'gc')
+
+    try:
+        tcnt += 1
+        print '\n### Test No. %2d ###\n' % tcnt
+        print 'Testing Invalid PIMDB config access. Should throw an exception.'
+        print 'bb:abcd last_sync_start: ', config.get_last_sync_start('bb', 'abcd')
+        print 'No Exception. WTF. Total Fail.'
+    except GoutConfigError, e:
+        print 'Hurrah. ', traceback.format_exc()
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'gc:ol sync_dir: ', config.get_sync_dir('gc', 'ol')
+    print 'gc:ol sync_dir setting to SYNC1WAY'
+    config.set_sync_dir('gc', 'ol', 'SYNC1WAY')
+    print 'gc:ol sync_dir: ', config.get_sync_dir('gc', 'ol')
+
+    try:
+        tcnt += 1
+        print '\n### Test No. %2d ###\n' % tcnt
+        print 'Try  Invalid value for sync_dir. Should throw Exception'
+        print 'gc:ol sync_dir: ', config.get_sync_dir('gc', 'ol')
+        print 'gc:ol sync_dir setting to GOOFY'
+        config.set_sync_dir('gc', 'ol', 'GOOFY')
+        print 'No Exception. WTF. Total Fail'
+    except GoutConfigError, e:
+        print 'Hurrah. ', traceback.format_exc()
+
+    tcnt += 1
+    print '\n### Test No. %2d ###\n' % tcnt
+    print 'bb:ol conflict resolve: ', config.get_conflict_resolve('bb', 'ol')
+    print 'bb:ol conflict_resolve to bb'
+    config.set_conflict_resolve('bb', 'ol', 'bb')
+    print 'bb:ol sync_dir: ', config.get_conflict_resolve('bb', 'ol')
+
+    try:
+        tcnt += 1
+        print '\n### Test No. %2d ###\n' % tcnt
+        print 'Try  Invalid value for conflict_resolve. Should throw Exception'
+        print 'bb:ol conflict resolve: ', config.get_conflict_resolve('bb', 'ol')
+        print 'bb:ol conflict_resolve to GUPPY'
+        config.set_conflict_resolve('bb', 'ol', 'GUPPY')
+        print 'No Exception. WTF. Total Fail'
+    except GoutConfigError, e:
+        print 'Hurrah. ', traceback.format_exc()
+        
+
+if __name__ == '__main__':
+    main()  
