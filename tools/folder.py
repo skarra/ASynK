cc6729f298f286a405541caa212734c0f76f4a4b
Sriram Karra <karra.etc@gmail.com>
2012-03-20 07:41:09 +0530

    New PIMDB abstract base class and first rewrite of Outlook wrapper

---
+++ tools/folder.py
@@ -0,0 +1,185 @@
+## 
+## Created       : Tue Mar 13 14:26:01 IST 2012
+## Last Modified : Mon Mar 19 13:41:14 IST 2012
+##
+## Copyright (C) 2012 Sriram Karra <karra.etc@gmail.com>
+##
+## Licensed under the GPL v3
+## 
+
+## 
+## This file defines an abstract base Contact class essentially as a way of
+## documenting what this application considers as a normalized contact
+## container.
+##
+
+from abc     import ABCMeta, abstractmethod
+from pimdb   import PIMDB, GoutInvalidPropValueError
+
+class Folder:
+    __metaclass__ = ABCMeta
+
+    ## We are borrowing the following constants from MAPI to identify our
+    ## folder types. I mean, why not...
+    CONTACT_t   = PR_IPM_CONTACT_ENTRYID     = 0x36D10102
+    NOTE_t      = PR_IPM_NOTE_ENTRYID        = 0x36D30102
+    TASK_t      = PR_IPM_TASK_ENTRYID        = 0x36D40102
+    APPT_t      = PR_IPM_APPOINTMENT_ENTRYID = 0x36D00102
+
+    valid_types = [CONTACT_t, NOTE_t, TASK_t, APPT_t]
+
+    def __init__ (self, db):
+        # Folders have properties that need to persist in the underlying
+        # database. We call them 'props'. These are defined and tracked in a
+        # single dictionary. Each of the derived classes will, of course, add
+        # to this stuff. Please note that there will be a mapping of these
+        # property names to a db-specific version that is actually stored.
+        self.props = {'itemid'      : None,
+                      'type'        : None,
+                      'name'        : None,
+                      'sync_tags'   : {},
+                      }
+        
+        # Then there are many class attributes that are needed to work with
+        # the programatically in the application, like pointers to the parent
+        # Folder and DB object, etc. Such attributes are tracked separately
+        # like any other object attributes
+
+        self.set_db(db)
+        self.set_dbid(db.get_dbid())
+        self.set_config(db.get_config())
+
+    @abstractmethod
+    def __str__ (self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def prep_sync_lists (self, destid, last_sync_stop, limit=0):
+        """Prepare and return a set of list of new, modified and deleted
+        entries in the current folder since the last sync to the corresponding
+        folder in the destination PIM Database identified by destid.
+
+        The format and stuff returned by this routine will be documented Real
+        Soon Now.
+
+        - destid is the two letter PIMDB id
+        - last_sync_stop was the end of the last sync cycle. It should be a
+        iso8601 formatted string - typically read out of the app_state.json
+        - limit can be used during debug time to limit the number of messages
+        processed/synched. If non-zero, only specified number of
+        messages/items will be processed
+        """
+
+        raise NotImplementedError
+
+    @abstractmethod
+    def insert_new_items (self, items):
+        """Insert new items into the database. entries is a list of objects
+        derived from pimdb.Item. This routine will ensure relevant fields are
+        fetched - which will invoke the source db implementation to get the
+        properies and use it to create an appropriate Item type object for
+        the destination folder and then store it in the folder
+        persistently."""
+
+        raise NotImplementedError
+
+    @abstractmethod
+    def bulk_clear_sync_flags (self, dbids):
+        """destid should be an array of PIMDB ids.
+
+        This routine will walk through the folder and delete any local storage
+        information that tracks the sync state. For e.g. this includes the
+        clearing custom flags / properties for contact entries that contain
+        the remote IDs in other databases
+
+        dbids is an array of two letter PIMDB identifiers are used in the sync
+        flags.
+        """
+
+        raise NotImplementedError
+
+    ##
+    ## Now the internal helper methods that will be used in the internal
+    ## implementetion of the class methods.
+    ## 
+
+    def _get_prop (self, key):
+        return self.props[key]
+
+    def _set_prop (self, key, val):
+        self.props[key] = val
+
+    def _append_to_prop (self, key, val):
+        """In the particular property value is an array, we would like to
+        append individual elements to the property value. this method does
+        exactly that."""
+
+        if not self.props[key]:
+            self.props[key] = [val]
+        else:
+            self.props[key].append(val)    
+
+    def _update_prop (self, prop, which, val):
+        """If a particular property value is a dictionary, we would like to
+        update the dictinary with a new mapping or alter an existing
+        mapping. This method does exactly that."""
+
+        if not self.props[prop]:
+            self.props[prop] = {which : val}
+        else:
+            self.props[prop].update({which : val})   
+
+    ##
+    ## Now on to the non-abstract methods
+    ##
+
+    def get_itemid (self):
+        return self._get_prop('itemid')
+
+    def set_itemid (self, val):
+        self._set_prop('itemid', val)
+
+    def get_config (self):
+        return self.config
+
+    def set_config (self, config):
+        self.config = config
+
+    def get_db (self):
+        return self.db
+
+    def set_db (self, db):
+        self.db = db
+
+    def get_type (self):
+        return self._get_prop('type')
+
+    def set_type (self, t):
+        if not t in self.valid_types:
+            raise GoutInvalidPropValueError(
+                'Invalid type in Folder:set_type : %s' % t)
+
+        self._set_prop('type', type)
+
+    def get_dbid (self):
+        return self.dbid
+
+    def set_dbid (self, dbid):
+        self.dbid = dbid
+
+    def is_contacts_folder (self):
+        return True if self.type == Folder.PR_IPM_CONTACT_ENTRYID else False
+
+    def is_notes_folder (self):
+        return True if self.type == Folder.PR_IPM_NOTE_ENTRYID else False
+
+    def is_tasks_folder (self):
+        return True if self.type == Folder.PR_IPM_TASK_ENTRYID else False
+
+    def is_appt_folder (self):
+        return True if self.type == Folder.PR_IPM_APPOINTMENT_ENTRYID else False
+
+## FIXME: This file needs extensive unit testing. There's quite a bit of
+## pseudo-repititive codet hat has been produced by manual cop-n-paste, which
+## is a certain recipe for silly typo errors that will not get flagged until
+## run time...
---
+++ tools/pimdb_ol.py
@@ -0,0 +1,306 @@
+##
+## Created       : Wed May 18 13:16:17 IST 2011
+## Last Modified : Tue Mar 20 07:06:44 IST 2012
+##
+## Copyright (C) 2011, 2012 Sriram Karra <karra.etc@gmail.com>
+##
+## Licensed under the GPL v3
+## 
+
+## This is an implementation of the Outlook PIMDB by extending the PIMDB
+## abstract base class
+
+## This file itself comes with some extensive unit tests. If you want to run
+## it from the command line, the usage is (from the current directory)
+## PYTHONPATH=../lib/:. python pimdb_ol.py
+
+import os, os.path, sys, logging, time, traceback
+from   datetime      import datetime
+
+import iso8601, base64
+import win32com.client, pywintypes, winerror
+from   win32com.mapi import mapi, mapitags, mapiutil
+
+import utils
+from   pimdb         import PIMDB, GoutInvalidPropValueError
+from   folder        import Folder
+from   folder_ol     import OLContactsFolder, OLTasksFolder, OLNotesFolder
+
+MOD_FLAG = mapi.MAPI_BEST_ACCESS
+
+class MessageStores:
+    """A container for all the message stores avaialble through the default
+    MAPI Login. It is a simple wrapper around a set of MessageStore objects
+    that makes available easy retrieval through either name, or the outlook
+    entry id.
+
+    There is no direct equivalent in any of the other DB
+    implementations. Hence this is not really 'exported' outside this file.
+    """
+
+    def __init__ (self, ol):
+        self.ol = ol
+
+        self._store_by_eid  = {}
+        self._store_by_name = {}
+        self._default_store = None
+
+        self._populate_stores()
+
+    def get_default_store (self):
+        return self._default_store
+
+    def get (self, name=None, eid=None):
+        """Fetch the MessageStore object for the specified entry id or the
+        name. The behaviour is undefined if both eid and name are specified in
+        the function call. If both name and eid are omitted in the function
+        call, the MessageStore object for the default message store is
+        returned.
+        """
+        if eid:
+            return self._store_by_eid[eid]
+        
+        if name:
+            return self._store_by_name[name]
+
+        return self.get_default_store()
+
+    def put (self, eid, name, default, store):
+        self._store_by_eid[eid]   = store
+        self._store_by_name[name] = store
+        if default:
+            self._default_store   = store
+
+    def _populate_stores (self):
+        """Walk through the message store table in Outlook, extract some key
+        properties of each store and keep track of them for later use."""
+
+        messagestorestable = self.ol.session.GetMsgStoresTable(0)
+
+        # This is where we need to add columns if we want to extract and store
+        # more information per Store
+        messagestorestable.SetColumns((mapitags.PR_ENTRYID,
+                                       mapitags.PR_DISPLAY_NAME_A,
+                                       mapitags.PR_DEFAULT_STORE),0)
+
+        i = 1
+        msgstores = []
+        while True:
+            rows = messagestorestable.QueryRows(1, 0)
+            # if this is the last row then stop
+            if len(rows) != 1:
+                break
+            row = rows[0]
+
+            (eid_tag, eid), (name_tag, name), (def_store_tag, def_store) = row
+            if True: #def_store:
+                # There is a real problem with OpenMsgStore() on non-default
+                # message stores. We do not know how to process these
+                # suckers - it just hangs. So for now, we just get ignore
+                # everything except the first
+                try:
+                    logging.debug('Msgstore #%2d: %s - Default. Processing.',
+                                  i, name)
+                    store = MessageStore(self.ol, eid, name, def_store)
+                    self.put(eid=eid, name=name, store=store,
+                             default=def_store)
+                except Exception, e:
+                    logging.debug('Error in opening message store. Skipping.')
+            else:
+                logging.debug('Msgstore #%2d: %s - skipped non-default one',
+                              i, name)
+            i += 1
+
+    def __str__ (self):
+        for name, store in self._store_by_name().itermsitems():
+            ret += str(store) + '\n'
+
+        return ret
+
+class MessageStore:
+    """A wrapper around an Outlook message store. This is not meant to be an
+    comprehensive wrapper around all the MAPI routines and variables. This is
+    just a convenience class to localize all store related data and the few
+    stor access mapi routines. More routines and properties will be added here
+    as they are needed in the applicatione.
+
+    Note that a Message store is what really contains multiple folders inside
+    them, and within Folders we have items of different kinds. As there is no
+    direct equivalent of this in other PIM databases, we are not exporting
+    this stuff outside of this file.
+    """
+
+    def __init__ (self, ol, eid, name, default):
+        self.ol      = ol
+        self.eid     = eid
+        self.name    = name
+        self.default = default
+        self.obj     = None
+
+        self.folders       = None
+
+        # This should really be done 'lazily' but let's go with the flow for
+        # now... FIXME
+        self._populate_folders()
+
+#        self.tasks_folders[0].print_key_stats()
+
+    def get_obj (self):
+        if self.obj:
+            return self.obj
+
+        # Open it.
+        logging.debug('OpenMsgStore on eid: %s', base64.b64encode(self.eid))
+        self.obj = self.ol.session.OpenMsgStore(0, self.eid, None,
+                                                (mapi.MDB_NO_DIALOG |
+                                                  MOD_FLAG))
+
+        return self.obj
+
+    def get_default_contacts_folder (self):
+        # Somewhat of a hack. There might be more than one contacts folder
+        # inside the store... FIXME
+        if self.contacts_folders and len(self.contacts_folders) > 0:
+            return self.contacts_folders[0]
+
+        return None
+
+    def get_inbox (self, msgstore):
+        inbox_id, c = msgstore.GetReceiveFolder("IPM.Note", 0)
+        inbox       = msgstore.OpenEntry(inbox_id, None, MOD_FLAG)
+
+        return inbox
+
+    def check_tag_error (self, tag):
+        if mapitags.PROP_TYPE(tag) == mapitags.PT_ERROR:
+            raise TypeError('got PT_ERROR: %16x' % tag)
+        elif mapitags.PROP_TYPE(tag) == mapitags.PT_BINARY:
+            pass
+
+    def get_folder_obj (self, tag, inbox):
+        """Return a tuple (entry_id, display_name, folder_obj) corresponding
+        to specific tag."""
+        hr, props = inbox.GetProps((tag), 0)
+        (tag0, eid)  = props[0]
+
+        # check for errors
+        self.check_tag_error(tag0)
+
+        name = self.get_entry_name(eid)
+        f    = self.get_obj().OpenEntry(eid, None, MOD_FLAG)
+
+        return (eid, name, f)
+
+    def _populate_folders (self):
+        if self.folders:
+            return self.folders
+
+        self.folders = {'contacts':[],'tasks':[],'notes':[],'appts':[],}
+
+        msgstore = self.get_obj()
+        inbox    = self.get_inbox(msgstore)
+
+        logging.debug('Building Folder list for Message Store: %s...',
+                      self.name)
+        #    def __init__ (self, db, entryid, name, fobj, msgstore):
+        try:
+            (eid, name, f) = self.get_folder_obj(Folder.PR_IPM_CONTACT_ENTRYID,
+                                                 inbox)
+            cf = OLContactsFolder(self.ol, eid, name, f, self)
+            self.folders['contacts'].append(cf)
+        except TypeError, e:
+            logging.debug('Actual exception: %s', e)
+            logging.info('No Contacts Folder for message store: %s',
+                         self.name)
+
+        try:
+            (eid, name, f) = self.get_folder_obj(Folder.PR_IPM_NOTE_ENTRYID,
+                                                 inbox)
+            nf = OLNotesFolder(self.ol, eid, name, f, self)
+            self.folders['notes'].append(nf)
+        except TypeError, e:
+            logging.debug('Actual exception: %s', e)
+            logging.info('No Notes Folder for message store: %s',
+                         self.name)
+
+        try:
+            (eid, name, f) = self.get_folder_obj(Folder.PR_IPM_TASK_ENTRYID,
+                                                 inbox)
+            tf = OLTasksFolder(self.ol, eid, name, f, self)
+            self.folders['tasks'].append(tf)
+        except TypeError, e:
+            logging.debug('Actual exception: %s', e)
+            logging.info('No Tasks Folder for message store: %s',
+                         self.name)
+
+        ## FIXME: We will have to do the above jig and dance for these
+        ## Calendars at some point.
+        ## folder types as well.
+
+    def get_ol_item (self, entryid):
+        return self.get_obj().OpenEntry(entryid, None, MOD_FLAG)
+
+    def get_entry_name (self, entryid):
+        item      = self.get_ol_item(entryid)
+        hr, props = item.GetProps([mapitags.PR_DISPLAY_NAME], mapi.MAPI_UNICODE)
+        tag, name = props[0] if props else (None, '')
+
+        return name
+
+    def __str__ (self):
+        ret = 'Message Store: %s. Total Folders: %d\n' % (self.name,
+                                                          len(self.folders))
+        for folder in self.folders:
+            ret += '\t' + str(folder) + '\n'
+
+        return ret
+
+class OLPIMDB(PIMDB):
+
+    def __init__ (self, config):
+        PIMDB.__init__(self, config)
+        self.mapi_initialize()
+        self.msgstores = MessageStores(self)
+
+    def __del__ (self):
+        logging.debug('Destroying mapi session...')
+        self.session.Logoff(0, 0, 0)
+
+    ##
+    ## First implementation of the abstract methods of PIMDB.
+    ##
+
+    def get_dbid (self):
+        """See the documentation in class PIMDB"""
+
+        return 'ol'
+
+    def set_folders (self):
+        """See the documentation in class PIMDB"""
+
+        self.folders = self.msgstores.folders
+
+    def set_def_folders (self):
+        """See the documentation in class PIMDB"""
+
+        self.def_folder['contacts'] = self.folders['contacts'][0]
+   
+    def set_sync_folders (self):
+        """See the documentation in class PIMDB"""
+
+        ## FIXME: This is obviously a hack and will need to be fixed. All the
+        ## folders can be synched, and it should really be left to the user...
+
+        self.sync_folders['contacts'].append(self.folders['contacts'][0])
+
+    ##
+    ## Now the non-abstract methods and internal methods
+
+    def mapi_initialize (self):
+        logging.debug('Initalizing MAPI...')
+
+        mapi.MAPIInitialize(None)
+        flags = (mapi.MAPI_EXTENDED | mapi.MAPI_USE_DEFAULT | MOD_FLAG)
+
+        logging.debug('Opening default profile in MAPI...')
+        self.session = mapi.MAPILogonEx(0, "", None, flags)
