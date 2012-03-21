be09d90cad4869f2b9ef21e027660128626b38e1
Sriram Karra <karra.etc@gmail.com>
2012-03-21 17:39:37 +0530

    Google Folder and Contact wrappers commit
    With this commit the main derived classes GCContact and GCContactFolder are
    defined, and useable to create and update contacts on the server. Even the
    prep_gc_sync_lists() routine is working as it was before the rewrite.
    
    This means that the Google Contacts side of things is about 75% ready for sync
    stuff to be implemented.

---
+++ tools/contact.py
@@ -0,0 +1,335 @@
+##
+## Created       : Tue Mar 13 14:26:01 IST 2012
+## Last Modified : Wed Mar 21 17:37:58 IST 2012
+##
+## Copyright (C) 2012 Sriram Karra <karra.etc@gmail.com>
+##
+## Licensed under the GPL v3
+##
+## This file defines an abstract base Contact class essentially as a way of
+## documenting what this application considers as a normalized contact
+## container. The "Copy Constructor" of this class will be the bridge across
+## contact entries from different databases.
+##
+
+from abc     import ABCMeta, abstractmethod
+from pimdb   import PIMDB
+from item    import Item
+
+class Contact(Item):
+    __metaclass__ = ABCMeta
+
+    def __init__ (self, folder, con=None):
+        Item.__init__(self, folder)
+
+        if con:
+            self.init_props_from_con(con)
+        else:
+            self.props.update({'firstname'    : None, 'company'      : None,
+                               'lastname'     : None, 'postal'       : None,
+                               'name'         : None, 'notes'        : [],
+                               'suffix'       : None, 'phone_home'   : [],
+                               'title'        : None, 'phone_work'   : [],
+                               'gender'       : None, 'phone_mob'    : [],
+                               'nickname'     : None, 'phone_other'  : [],
+                               'birthday'     : None, 'phone_prim'   : None,
+                               'anniv'        : None, 'fax_home'     : [],
+                               'web_home'     : [],   'fax_work'     : [],
+                               'web_work'     : [],   'fax_prim'     : None,
+                               'web_prim'     : None, 'email_home'   : [],
+                               'dept'         : None, 'email_work'   : [],
+                               'fileas'       : None, 'email_other'  : [],
+                               'prefix'       : None, 'email_prim'   : None,
+                               'im'           : {},
+                               })
+
+    ##
+    ## Now onto the non-abstract methods. We do not want to use method
+    ## generators - it makes the code harder to read and maintain. It would be
+    ## simple to enforce range checking and other validation when it's in
+    ## plain english like so.
+    ##
+
+    def init_props_from_con (self, con, excl_itemid=True):
+        """Make a deepcopy of all the item properties from con into the props
+        dictionary, utilizing the appropriate get_ and set_ routines.
+
+        By default the itemid field is excluded from the copy as the most
+        common usecase is to make a copy of the contact fields in one
+        database format into another database format - i.e. Create an
+        OLContact object from the fields of a GCContact object. In such an
+        instance the itemid field in the destination should really be
+        generated from a store in the database on creation, or through some
+        other such means."""
+
+        prop_names = con.get_prop_names()
+        if excl_itemid:
+            prop_names.remove('itemid')
+
+        for prop in prop_names:
+            get_method = 'get_%s' % prop
+            set_method = 'set_%s' % prop
+
+            val = copy.deepcopy(getattr(con, get_method)())
+            print 'setting value (', val, ') using method ', set_method
+            getattr(self, set_method)(val)
+
+    def get_prop_names (self):
+        return self.props.keys()
+
+    def get_firstname (self):
+        return self._get_prop('firstname')
+
+    def set_firstname (self, val):
+        self._set_prop('firstname', val)
+        self.update_fullname()
+
+    def get_lastname (self):
+        return self._get_prop('lastname')
+
+    def set_lastname (self, val):
+        self._set_prop('lastname', val)
+        self.update_fullname()
+
+    def update_fullname (self):
+        pr = self.get_prefix()
+        fn = self.get_firstname()
+        ln = self.get_lastname()
+        su = self.get_suffix()
+
+        name = pr if pr else ''
+        name += fn if fn else ''
+        name += ln if ln else ''
+        name += su if su else ''
+
+        self._set_prop('name', name)
+
+    def get_name (self):
+        return self._get_prop('name')
+
+    def set_name (self, val):
+        self._set_prop('name', val)
+        return val
+
+    def get_prefix (self):
+        return self._get_prop('prefix')
+
+    def set_prefix (self, val):
+        return self._set_prop('prefix', val)
+
+    def get_suffix (self):
+        return self._get_prop('suffix')
+
+    def set_suffix (self, val):
+        return self._set_prop('suffix', val)
+
+    def get_fileas (self):
+        return self._get_prop('fileas')
+
+    def set_fileas (self, val):
+        return self._set_prop('fileas', val)
+
+    def get_gender (self):
+        return self._get_prop('gender')
+
+    def set_gender (self, val):
+        return self._set_prop('gender', val)
+
+    def get_nickname (self):
+        return self._get_prop('nickname')
+
+    def set_nickname (self, val):
+        return self._set_prop('nickname', val)
+
+    def get_birthday (self):
+        return self._get_prop('birthday')
+
+    def set_birthday (self, val):
+        return self._set_prop('birthday', val)
+
+    def get_anniv (self):
+        return self._get_prop('anniv')
+
+    def set_anniv (self, val):
+        return self._set_prop('anniv', val)
+
+    def get_web_prim (self):
+        return self._get_prop('web_prim')
+
+    def set_web_prim (self, val):
+        return self._set_prop('web_prim', val)
+
+    def get_web_home (self):
+        return self._get_prop('web_home')
+
+    def set_web_home (self, val):
+        return self._set_prop('web_home', val)
+
+    def add_web_home (self, val):
+        return self._append_to_prop('web_home', val)
+
+    def get_web_work (self):
+        return self._get_prop('web_work')
+
+    def set_web_work (self, val):
+        return self._set_prop('web_work', val)
+
+    def add_web_work (self, val):
+        return self._append_to_prop('web_work', val)
+
+    def get_company (self):
+        return self._get_prop('company')
+
+    def set_company (self, val):
+        return self._set_prop('company', val)
+
+    def get_title (self):
+        return self._get_prop('title')
+
+    def set_title (self, val):
+        return self._set_prop('title', val)
+
+    def get_dept (self):
+        return self._get_prop('dept')
+
+    def set_dept (self, val):
+        if val:
+            return self._set_prop('dept', val)
+
+    def get_postal (self):
+        return self._get_prop('postal')
+
+    def set_postal (self, val):
+        return self._set_prop('postal', val)
+
+    def get_notes (self):
+        return self._get_prop('notes')
+
+    def set_notes (self, val):
+        return self._set_prop('notes', val)
+
+    def add_notes (self, val):
+        return self._append_to_prop('notes', val)
+
+    def get_email_prim (self):
+        return self._get_prop('email_prim')
+
+    def set_email_prim (self, val):
+        return self._set_prop('email_prim', val)
+
+    def get_email_home (self):
+        return self._get_prop('email_home')
+
+    def set_email_home (self, val):
+        return self._set_prop('email_home', val)
+
+    def add_email_home (self, val):
+        return self._append_to_prop('email_home', val)
+
+    def get_email_work (self):
+        return self._get_prop('email_work')
+
+    def set_email_work (self, val):
+        return self._set_prop('email_work', val)
+
+    def add_email_work (self, val):
+        return self._append_to_prop('email_work', val)
+
+    def get_email_other (self):
+        return self._get_prop('email_other')
+
+    def set_email_other (self, val):
+        return self._set_prop('email_other', val)
+
+    def add_email_other (self, val):
+        return self._append_to_prop('email_other', val)
+
+    def get_phone_home (self):
+        return self._get_prop('phone_home')
+
+    def set_phone_home (self, val):
+        return self._set_prop('phone_home', val)
+
+    def add_phone_home (self, val):
+        return self._append_to_prop('phone_home', val)
+
+    def get_phone_work (self):
+        return self._get_prop('phone_work')
+
+    def set_phone_work (self, val):
+        return self._set_prop('phone_work', val)
+
+    def add_phone_work (self, val):
+        return self._append_to_prop('phone_work', val)
+
+    def get_phone_mob (self):
+        return self._get_prop('phone_mob')
+
+    def set_phone_mob (self, val):
+        return self._set_prop('phone_mob', val)
+
+    def add_phone_mob (self, val):
+        return self._append_to_prop('phone_mob', val)
+
+    def get_phone_other (self):
+        return self._get_prop('phone_other')
+
+    def set_phone_other (self, val):
+        return self._set_prop('phone_other', val)
+
+    def add_phone_other (self, val):
+        return self._append_to_prop('phone_other', val)
+
+    ## FIXME: Not really sure if this way of handling the primary phone will
+    ## really cut it. Need to focus some testing on this.
+
+    def get_phone_prim (self):
+        return self._get_prop('phone_prim')
+
+    def set_phone_prim (self, val):
+        return self._set_prop('phone_prim', val)
+
+    def get_fax_home (self):
+        return self._get_prop('fax_home')
+
+    def set_fax_home (self, val):
+        return self._set_prop('fax_home', val)
+
+    def add_fax_home (self, val):
+        return self._append_to_prop('fax_home', val)
+
+    def get_fax_work (self):
+        return self._get_prop('fax_work')
+
+    def set_fax_work (self, val):
+        return self._set_prop('fax_work', val)
+
+    def add_fax_work (self, val):
+        return self._append_to_prop('fax_work', val)
+
+    ## FIXME: Do something about the fax_prim field and how to manage that.
+
+    def get_fax_prim (self):
+        return self._get_prop('fax_prim')
+
+    def set_fax_prim (self, val):
+        return self._set_prop('fax_prim', val)
+
+    def get_im (self, which=None):
+        all_ims = self._get_prop('im')
+        if not which:
+            return all_ims
+        else:
+            return all_ims[which]
+
+    def set_im (self, val):
+        return self._set_prop('im', val)
+
+    def add_im (self, which, val):
+        return self._update_prop('im', which, val)
+
+
+## FIXME: This file needs extensive unit testing. There's quite a bit of
+## pseudo-repititive codet hat has been produced by manual cop-n-paste, which
+## is a certain recipe for silly typo errors that will not get flagged until
+## run time...
---
+++ tools/item.py
@@ -0,0 +1,158 @@
+##
+## Created	     : Tue Mar 13 14:26:01 IST 2012
+## Last Modified : Wed Mar 21 17:36:39 IST 2012
+##
+## Copyright (C) 2012 Sriram Karra <karra.etc@gmail.com>
+##
+## Licensed under the GPL v3
+##
+## This file defines an abstract base Item class. Contact, Task, Appointment
+## and Note can / will be derived from this base class and will reside in
+## their own files.
+
+from abc     import ABCMeta, abstractmethod
+from pimdb   import PIMDB, GoutInvalidPropValueError
+from folder  import Folder
+
+class Item:
+    """A generic PIM item - can be a Contact, Task, Note, or Appointment."""
+
+    __metaclass__ = ABCMeta
+
+    valid_types   = [Folder.CONTACT_t, Folder.NOTE_t, Folder.TASK_t,
+                     Folder.APPT_t]
+
+    def __init__ (self, folder):
+        # Items have properties that need to persist in the underlying
+        # database. We call them 'props'. These are defined and tracked in a
+        # single dictionary. Each of the derived classes will, of course, add
+        # to this stuff.
+        self.props = {'itemid'      : None,
+                      'type'        : None,
+                      'sync_tags'   : {},
+                      }
+
+        # Then there are many class attributes that are needed to work with
+        # the programatically in the application, like pointers to the parent
+        # Folder and DB object, etc. Such attributes are tracked separately
+        # like any other object attributes
+
+        self.set_folder(folder)
+        self.set_db(folder.get_db())
+        self.set_dbid(folder.get_dbid())
+        self.set_config(folder.get_config())
+
+    ##
+    ## First the abstract methods
+    ##
+
+    @abstractmethod
+    def save (self):
+        """Make this item persistent in the underlying database. On success
+        this method should set the itemid field if it has changed, and return
+        the new value. On failure None is returned."""
+
+        raise NotImplementedError
+
+    ##
+    ## Now the internal helper methods that will be used in the internal
+    ## implementetion of the class methods.
+    ##
+
+    def _get_prop (self, key):
+        return self.props[key]
+
+    def _set_prop (self, key, val):
+        self.props.update({key : val})
+        return val
+
+    def _append_to_prop (self, key, val):
+        """In the particular property value is an array, we would like to
+        append individual elements to the property value. this method does
+        exactly that."""
+
+        if not self.props[key]:
+            self.props[key] = [val]
+        else:
+            self.props[key].append(val)
+
+    def _update_prop (self, prop, which, val):
+        """If a particular property value is a dictionary, we would like to
+        update the dictinary with a new mapping or alter an existing
+        mapping. This method does exactly that."""
+
+        if not self.props[prop]:
+            self.props[prop] = {which : val}
+        else:
+            self.props[prop].update({which : val})
+
+    ##
+    ## Finally, the get_ and set_ methods.
+    ##
+
+    ## First the non-props
+
+    def get_folder (self):
+        return self.folder
+
+    def set_folder (self, val):
+        self.folder = val
+
+    def get_db (self):
+        return self.db
+
+    def set_db (self, val):
+        self.db = val
+
+    def get_config (self):
+        return self.config
+
+    def set_config (self, config):
+        self.config = config
+
+    ## Now, the item properties
+
+    def get_itemid (self):
+        return self._get_prop('itemid')
+
+    def set_itemid (self, val):
+        self._set_prop('itemid', val)
+
+    def get_dbid (self):
+        return self.dbid
+
+    def set_dbid (self, val):
+        self.dbid = val
+
+    def get_type (self):
+        return self._get_prop('type')
+
+    def set_type (self, val):
+        if not val in self.valid_types:
+            raise GoutInvalidPropValueError('Invalid type: %s', val)
+
+        self._set_prop('type', val)
+
+    def get_sync_tags (self, destid=None):
+        """Return the sync tag corresponding to specified DBID: destid. If
+        destid is None, the full dictionary of sync tags is returned to the
+        uesr."""
+
+        tags = self._get_prop('sync_tags')
+        return tags[destid] if destid else tags
+
+    def set_sync_tags (self, val):
+        """While this is not anticipated to be used much, this routine gives
+        the flexibility to set the entire sync_tags dictionary
+        wholesale. Potential use cases include clearing all existing values,
+        etc."""
+
+        self._set_props('sync_tags', val)
+
+    def update_sync_tags (self, destid, val):
+        """Update the specified sync tag with given value. If the tag does not
+        already exist an entry is created."""
+
+        self._update_prop('sync_tags', destid, val)
+
+## FIXME: This file needs extensive unit testing
