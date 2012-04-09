b0cef009df2ec109f37f84232f1d019d2c808a56
Sriram Karra <karra.etc@gmail.com>
2012-04-09 17:39:11 +0530

    Move testing code from *_ol.py files to a test/test_ol.py file
    This is in keeping with the desired change to consolidate all
    the testing code in the test/ directory and prevent polluting the
    main feature-bearing code.

--- tools/contact_ol.py
+++ tools/contact_ol.py
@@ -13,13 +13,6 @@ import string
 import base64, logging, os, re, sys, traceback, utils
 from   datetime import datetime
 
-if __name__ == "__main__":
-    ## Being able to fix the sys.path thusly makes is easy to execute this
-    ## script standalone from IDLE. Hack it is, but what the hell.
-    DIR_PATH    = os.path.abspath(os.path.dirname(os.path.realpath('../Gout')))
-    EXTRA_PATHS = [os.path.join(DIR_PATH, 'lib')]
-    sys.path = EXTRA_PATHS + sys.path
-
 from   contact import Contact
 from   win32com.mapi import mapitags as mt
 from   win32com.mapi import mapi
@@ -725,62 +718,3 @@ class OLContact(Contact):
                 logging.error('Field %s not found', field)
 
         return ar
-
-def main (argv=None):
-    tests = TestOLContact()
-    
-    tests.test_read_emails('AAAAADWE5+lnNclLmn8GpZUD04fE7C0A')
-
-    # tests.test_new_contact()
-    #    tests.test_sync_status()
-
-class TestOLContact:
-    def __init__ (self):
-        from state import Config
-        from pimdb_ol import OLPIMDB
-
-        logging.debug('Getting started... Reading Config File...')
-
-        self.config = Config('../app_state.json')
-        self.ol     = OLPIMDB(self.config)
-        self.deff   = self.ol.get_def_folder()
-
-        print "\nHurrah: Name is: ", self.deff.get_name()
-
-    def test_new_contact (self):
-        c = OLContact(self.deff)
-        c.set_name('Supeman')
-        c.set_gender('Male')
-        c.set_notes('This is a second test contact')
-        c.save()
-
-    def test_read_emails (self, itemid):
-        eid = base64.b64decode(itemid)
-        olcf = self.deff
-        
-        prop_tag = olcf.get_proptags().valu('ASYNK_PR_EMAIL_1')
-        store    = olcf.get_msgstore()
-        item     = store.get_obj().OpenEntry(eid, None, mapi.MAPI_BEST_ACCESS)
-
-        hr, props = item.GetProps([prop_tag], mapi.MAPI_UNICODE)
-        (tag, val) = props[0]
-        if mt.PROP_TYPE(tag) == mt.PT_ERROR:
-            print 'Prop_Tag (0x%16x) not found. Tag: 0x%16x' % (prop_tag,
-                                                                (tag % (2**64)))
-        else:
-            print 'Email address found: ', val
-
-    def test_sync_status (self):
-        from   sync       import SyncLists
-        sl = SyncLists(self.deff, 'gc')
-        self.deff.prep_sync_lists('gc', sl)
-
-if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.DEBUG)
-    try:
-        main()
-    except Exception, e:
-        print 'Caught Exception... Hm. Need to cleanup.'
-        print 'Full Exception as here:', traceback.format_exc()
-
-## FIXME: Needs more thorough unit testing.
