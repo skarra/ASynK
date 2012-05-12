cef0bf5d1cece566b5c2293c9bd639030b8a9425
Sriram Karra <karra.etc@gmail.com>
2012-05-12 20:54:34 +0530

    Source for the main website hosted on GAE

---
+++ gae/js/asynk.js
@@ -0,0 +1,45 @@
+// 
+// Created       : Sat May 05 13:15:20 IST 2012
+// Last Modified : Sat May 12 17:54:57 IST 2012
+//
+// Copyright (C) 2012, Sriram Karra <karra.etc@gmail.com>
+// All Rights Reserved
+//
+// Licensed under the GNU AGPL v3
+// 
+
+function hideAllSheetDivs() {
+    $("#sheet").children().css({display : "none"})
+}
+
+// Register callbacks to handle specific events on our main UI.
+function addFormHandlers () {
+    console.log('addFormHandlers');
+
+    $("#home").mouseenter(function () {
+	hideAllSheetDivs();
+	$("#home_text").css({display : "block"});
+    })
+
+    $("#downloads").mouseenter(function () {
+	hideAllSheetDivs()
+	$("#downloads_text").css({display : "block"})
+    })
+
+    $("#doc").mouseenter(function () {
+	hideAllSheetDivs()
+	$("#doc_text").css({display : "block"});
+    })
+
+    $("#about").mouseenter(function () {
+	hideAllSheetDivs()
+	$("#about_text").css({display : "block"})
+    })
+}
+
+function onLoad () {
+    // Initialize the database if available
+    addFormHandlers();
+}
+
+jQuery(onLoad);
