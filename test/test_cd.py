b79522436ebfce771f7ac4947ec4a23ba3eaed34
Sriram Karra <karra.etc@gmail.com>
2013-04-02 19:43:31 +0530

    Simple datastore initiailzation and test harness using caldavclientlibrary

---
+++ test/test_cd.py
@@ -0,0 +1,49 @@
+##
+## Created       : Tue Apr 02 13:32:55 IST 2013
+## Last Modified : Tue Apr 02 19:33:20 IST 2013
+##
+## Copyright (C) 2013 Sriram Karra <karra.etc@gmail.com>
+##
+## This file is part of ASynK
+##
+## ASynK is free software: you can redistribute it and/or modify it under
+## the terms of the GNU Affero General Public License as published by the
+## Free Software Foundation, version 3 of the License
+##
+## ASynK is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+## License for more details.
+##
+## You should have a copy of the license in the doc/ directory of ASynK.  If
+## not, see <http://www.gnu.org/licenses/>.
+##
+## This file is used for some poking around with the BBDB code of ASynK -
+## stuff like print a contact from a bbdb database, test new changes to pimdb
+## or folder code, etc. Essentially some test routines that are not really
+## unit tests. code often moves from here to the unittest directory (gold/)
+## after a while
+
+import logging, os, sys
+
+## Being able to fix the sys.path thusly makes is easy to execute this
+## script standalone from IDLE. Hack it is, but what the hell.
+CUR_DIR           = os.path.abspath('')
+ASYNK_BASE_DIR    = os.path.abspath('..')
+EXTRA_PATHS       = [os.path.join(ASYNK_BASE_DIR, 'lib'),
+                     os.path.join(ASYNK_BASE_DIR, 'asynk'),]
+sys.path          = EXTRA_PATHS + sys.path
+
+from state         import Config
+from pimdb_cd      import CDPIMDB
+
+def main (argv=None):
+    conf = Config(os.path.join('..', 'config.json'), 'state.test.json')
+    user = raw_input('Enter Username:')
+    pw   = raw_input('Password:')
+    cd   = CDPIMDB(conf, 'localhost:8008', user, pw)
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.DEBUG)
+    main()
+
