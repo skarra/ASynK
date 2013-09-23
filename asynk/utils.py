6c5b4280c34979c68637acaf22623738cf1e3f22
Sriram Karra <karra.etc@gmail.com>
2013-09-23 14:37:38 +0530

    Bumping up version to v1.1.0+ for development

--- asynk/utils.py
+++ asynk/utils.py
@@ -42,7 +42,7 @@ def pytime_to_yyyy_mm_dd (pyt):
         return ('%04d-%02d-%02d' % (pyt.year, pyt.month, pyt.day))
 
 ## Some Global Variables to get started
-asynk_ver = 'v1.1.0'
+asynk_ver = 'v1.1.0+'
 
 def asynk_ver_str ():
     return 'ASynK %s' % asynk_ver
